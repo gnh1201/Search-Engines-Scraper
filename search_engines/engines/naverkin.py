c205c2cf6e46424482e887e07c78c66aeef543c6
Namhyeon, Go <gnh1201@gmail.com>
2020-10-22 19:13:45 +0900

    Update naverkin.py

--- search_engines/engines/naverkin.py
+++ search_engines/engines/naverkin.py
@@ -5,24 +5,24 @@ from ..config import PROXY, TIMEOUT, FAKE_USER_AGENT
 class Naverkin(SearchEngine):
     '''Searches kin.naver.com'''
     def __init__(self, proxy=PROXY, timeout=TIMEOUT):
-        super(Bing, self).__init__(proxy, timeout)
+        super(Naverkin, self).__init__(proxy, timeout)
         self._base_url = 'https://kin.naver.com'
         self.set_headers({'User-Agent':FAKE_USER_AGENT})
 
     def _selectors(self, element):
         '''Returns the appropriate CSS selector.'''
         selectors = {
-            'url': 'a[href]', 
-            'title': 'a', 
-            'text': 'p', 
-            'links': 'ul.basic1 > li', 
+            'url': 'a[href]',
+            'title': 'a',
+            'text': 'dd:nth-child(3)',
+            'links': 'ul.basic1 > li',
             'next': '.s_paging a:last-child'
         }
         return selectors[element]
     
     def _first_page(self):
         '''Returns the initial page and query.'''
-        url = u'{}/search/list.nhn?sort=none&query={}&section=kin&page=1'.format(self._base_url, self._query)
+        url = u'{}/search/list.nhn?sort=date&query={}&section=kin'.format(self._base_url, self._query)
         return {'url':url, 'data':None}
     
     def _next_page(self, tags):
@@ -31,5 +31,5 @@ class Naverkin(SearchEngine):
         next_page = self._get_tag_item(tags.select_one(selector), 'href')
         url = None
         if next_page:
-            url = next_page
+            url = (self._base_url + next_page) 
         return {'url':url, 'data':None}
